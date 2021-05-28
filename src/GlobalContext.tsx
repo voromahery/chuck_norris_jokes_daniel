d1a1576232efb46873edeccda4a139a8c68f38ae
student <daniel.her@onja.org>
2021-05-28 15:10:50 +0300

    refactor: using simple context

--- src/GlobalContext.tsx
+++ src/GlobalContext.tsx
@@ -1,10 +1,8 @@
-import { stat } from 'fs'
-import React, { createContext, useReducer, useEffect, useState } from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 
-type State = {
+interface JokeState {
   isLoading: boolean
   jokeData: any
-  dispatch: React.Dispatch<any>
   firstName: string
   lastName: string
   setFirstName: React.Dispatch<React.SetStateAction<string>>
@@ -14,10 +12,9 @@ type State = {
   setCategory: React.Dispatch<React.SetStateAction<string>>
 }
 
-let initialState: State = {
+let initialState: JokeState = {
   isLoading: true,
   jokeData: {},
-  dispatch: () => null,
   firstName: '',
   lastName: '',
   setFirstName: () => null,
@@ -27,7 +24,7 @@ let initialState: State = {
   setCategory: () => null,
 }
 
-type JokeProperties = {
+interface JokeProperties {
   type: string
   value: {
     id: number
@@ -36,47 +33,23 @@ type JokeProperties = {
   }
 }
 
-type Action =
-  | { type: 'RESOLVED'; payload: JokeProperties }
-  | { type: 'LOADING' }
-
-function reducer(state: State = initialState, action: Action) {
-  switch (action.type) {
-    case 'LOADING':
-      return {
-        ...state,
-        isLoading: true,
-      }
-    case 'RESOLVED':
-      return {
-        ...state,
-        isLoading: false,
-        jokeData: action.payload,
-      }
-
-    default:
-      return state
-  }
-}
-
 const Context = createContext(initialState)
 
 const GlobalContext: React.FC = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [isLoading, setIsLoading] = useState(true)
+  const [jokeData, setJokeData] = useState({})
   const [category, setCategory] = useState('')
   const [firstName, setFirstName] = useState('Chuck')
   const [lastName, setLastName] = useState('Norris')
   const jokeUrl = `http://api.icndb.com/jokes/random?firstName=${firstName}&lastName=${lastName}&${category}`
 
   const fetchJoke = async () => {
-    let isLoading = true
-    dispatch({ type: 'LOADING' })
+    setIsLoading(true)
     const getJoke = await fetch(jokeUrl)
     const data = await getJoke.json()
-    dispatch({ type: 'RESOLVED', payload: data.value })
-    return (isLoading = false)
+    setJokeData(data.value)
+    setIsLoading(false)
   }
-  console.log(state.jokeData.categories)
   useEffect(() => {
     fetchJoke()
   }, [firstName, lastName])
@@ -84,9 +57,8 @@ const GlobalContext: React.FC = ({ children }) => {
   return (
     <Context.Provider
       value={{
-        isLoading: state.isLoading,
-        jokeData: state.jokeData,
-        dispatch,
+        jokeData,
+        isLoading,
         firstName,
         lastName,
         setFirstName,
