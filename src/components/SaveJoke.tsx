32e364456d1a22af09462a25652c0db5477a5aea
student <daniel.her@onja.org>
2021-06-01 14:55:17 +0300

    clean: removes unecessary code

--- src/components/SaveJoke.tsx
+++ src/components/SaveJoke.tsx
@@ -83,9 +83,6 @@ const NumberOfJokes = () => {
         <button className={counterButton} id='decrement' onClick={decrement}>
           -
         </button>
-        <label htmlFor='jokeCount' style={{ display: 'none' }}>
-          Joke Count
-        </label>
         <input
           type='number'
           name='jokeCount'
