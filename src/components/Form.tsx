567dfcaf957ed0abec06884ae574a744edb93c66
student <daniel.her@onja.org>
2021-06-01 07:29:06 +0300

    refactor: creates variables for class conditions in order to make the code clean

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -11,17 +11,28 @@ const Form = () => {
     setLastName,
     setCategory,
   } = useContext(Context)
+
   const categoryUrl = `http://api.icndb.com/categories`
-  const [changingName, setChangingName] = useState(
+  const [dynamicChangingName, setDynamicChangingName] = useState(
     firstName ? `${firstName} ${lastName}` : ''
   )
+
+  const renamingCodition = firstName
+    ? `${firstName} ${lastName}`
+    : 'Chuck Norris'
   const [categoryList, setCategoryList] = useState([])
   const [openDropDown, setOpenDropDown] = useState(false)
   const [textToDisplay, setTextToDislay] = useState('Categories')
 
-  const getRandomJoke = () => {
+  const fetchingJokeCategory = async () => {
+    const response = await fetch(categoryUrl)
+    const data = await response.json()
+    setCategoryList(data.value)
+  }
+
+  const getRandomJokeByName = () => {
     fetchJoke()
-    let name = changingName.split(' ')
+    let name = dynamicChangingName.split(' ')
     if (name.length === 2) {
       setFirstName(name[0])
       setLastName(name[1])
@@ -32,23 +43,19 @@ const Form = () => {
     }
   }
 
-  const fetchingCategory = async () => {
-    const response = await fetch(categoryUrl)
-    const data = await response.json()
-    setCategoryList(data.value)
-  }
-
   const toggleDropDown = () => {
     setOpenDropDown(!openDropDown)
   }
 
-  const renaming = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setChangingName(e.target.value)
+  const renamingCharacter = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setDynamicChangingName(e.target.value)
   }
 
   const selectCategory = (e: any) => {
     toggleDropDown()
     const optionValue = e.target.value
+
+    // Change the first character of the category into capital
     const capitalizedFirstLetter =
       optionValue.charAt(0).toUpperCase() + optionValue.slice(1)
     setCategory(optionValue && `limitTo=[${optionValue}]`)
@@ -56,23 +63,27 @@ const Form = () => {
   }
 
   useEffect(() => {
-    fetchingCategory()
+    fetchingJokeCategory()
   }, [])
+
+  // Class conditions
+  const customSelectTriggerClass = openDropDown
+    ? 'custom__select--trigger open__trigger '
+    : 'custom__select--trigger closed__trigger'
+
+  const selectedOptionClass = category
+    ? 'custom__select'
+    : 'custom__select option__selected'
+
+  const triggerCategoryClass = openDropDown
+    ? 'category__list open'
+    : 'category__list closed'
 
   return (
     <div className='form__wrapper'>
       <div className='customized__select--container'>
-        <div
-          className={
-            category ? 'custom__select' : 'custom__select option__selected'
-          }>
-          <div
-            className={
-              openDropDown
-                ? 'custom__select--trigger open__trigger '
-                : 'custom__select--trigger closed__trigger'
-            }
-            onClick={toggleDropDown}>
+        <div className={selectedOptionClass}>
+          <div className={customSelectTriggerClass} onClick={toggleDropDown}>
             {!openDropDown ? (
               <div className='trigger__closed--text'>
                 {textToDisplay ? textToDisplay : 'Categories'}
@@ -89,10 +100,7 @@ const Form = () => {
               <path d='M1742.9 37.2l-.3-.3c-49.6-49.4-129.8-49.2-179.2.3L890.5 710.4v.2L216.7 37.4C193 13.9 160.9.7 127.5.6 57.3.4.2 57.2 0 127.5c-.1 33.8 13.2 66.3 37.1 90.2l768 766.3c50.1 45.8 127.3 44.2 175.4-3.7l762.4-763c49.5-49.8 49.5-130.3 0-180.1z'></path>
             </svg>
           </div>
-          <ul
-            className={
-              openDropDown ? 'category__list open' : 'category__list closed'
-            }>
+          <ul className={triggerCategoryClass}>
             {categoryList.map((category) => (
               <li className='customized__select--option' key={category}>
                 <button
@@ -115,20 +123,16 @@ const Form = () => {
           type='text'
           id='renaming'
           className='renaming__input'
-          value={changingName}
-          onChange={renaming}
+          value={dynamicChangingName}
+          onChange={renamingCharacter}
         />
         <label htmlFor='renaming' className='impersonating__text'>
-          Impersonate {firstName ? `${firstName} ${lastName}` : 'Chuck Norris'}
+          Impersonate {renamingCodition}
         </label>
       </div>
-      <button className='draw__joke--button' onClick={getRandomJoke}>
+      <button className='draw__joke--button' onClick={getRandomJokeByName}>
         Draw a random{' '}
-        {changingName.trim()
-          ? changingName
-          : firstName
-          ? `${firstName} ${lastName}`
-          : 'Chuck Norris'}{' '}
+        {dynamicChangingName.trim() ? dynamicChangingName : renamingCodition}{' '}
         Joke
       </button>
     </div>
